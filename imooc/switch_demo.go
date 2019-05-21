76289629a58409d95de0c3bc024d75d4d795fc38
TesterCC <testerlyx@foxmail.com>
2019-05-21 10:47:08 +0800

    add: switch condition demo

---
+++ imooc/switch_demo.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+// switch后可以没有表达式
+func grade(score int) string{
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	}
+	return g
+}
+
+
+//普通的switch， go中的switch会自动break，除非使用fallthrough
+func eval(a, b int, op string) int {
+	var result int
+	switch op {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "*":
+		result = a * b
+	case "/":
+		result = a / b
+	default:
+		panic("unsupported operator:"+op)
+	}
+	return result
+}
+
+
+
+func main() {
+	fmt.Println(
+		grade(0),
+		grade(59),
+		grade(60),
+		grade(82),
+		grade(100),
+		//grade(-3),
+		)
+
+	fmt.Println(
+		eval(3,4, "+"),
+		eval(7,6, "-"),
+		eval(5,6, "*"),
+		eval(6,3, "/"),
+		//eval(6,3, "%"),
+		)
+}
