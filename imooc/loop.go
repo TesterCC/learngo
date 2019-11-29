d8fdfbab65db124c5a9c043056222b686fe0f3c4
TesterCC <testerlyx@foxmail.com>
2019-11-29 16:06:21 +0800

    add: go loop notes

--- imooc/loop.go
+++ imooc/loop.go
@@ -1,9 +1,95 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 /*
-2-6 循环  0:47
+2-6 循环
 */
 
+// 功能：把整数转成二进制数
+func convertToBin(n int) string {
+	result := ""
+	if n==0 {
+		return "0"
+	}
+
+	for ; n>0 ; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+// 功能：把整数转成二进制数v2
+func convertToBin_v2(v int) string {
+	result := ""
+
+	if v==0 {
+		return "0"
+	}
+
+	// 省略初始条件，相当于while
+	for ; v > 0; v /= 2 {
+		result = strconv.Itoa(v%2) + result
+	}
+
+	return result
+}
+
+
+// 用另一种方法read文件并print, 逐行读取
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {   //nil是一个重要的预先定义好的标识符，是许多种类型的零值表示  https://www.jianshu.com/p/174aa63b2cc5
+		panic(err)   // 终止程序，然后报错
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+
+}
+
+func forever() {
+
+	// 注意，这是一个死循环
+	for {
+		fmt.Println("abc")
+	}
+}
+
 func main() {
+	fmt.Println(
+		convertToBin(5), // 101
+		convertToBin(13), // 1011 -> 1101
+		convertToBin(777777),
+		convertToBin(0),  // 空字符串没有显示，因为没有对0的处理
+		)
+
+	printFile("abc.txt")
+
+	fmt.Println(
+		convertToBin_v2(5), // 101
+		convertToBin_v2(13), // 1011 -> 1101
+		convertToBin_v2(777777),
+		convertToBin_v2(0),  // 空字符串没有显示，因为没有对0的处理
+	)
+
+	//forever()
 	
 }
+
+/*
+
+cd ~/imooc.com/learngo/imooc
+
+go run loop.go
+
+*/
