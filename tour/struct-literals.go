82f4c00aa9215f293d36391b8a1bf74422cf0ef7
TesterCC <testerlyx@foxmail.com>
2020-03-27 01:32:11 +0800

    update: a tour of go, struct demo

--- tour/struct-literals.go
+++ tour/struct-literals.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 http://127.0.0.1:3999/moretypes/5
 https://tour.go-zh.org/moretypes/5
@@ -13,6 +15,17 @@ https://tour.go-zh.org/moretypes/5
 特殊的前缀 & 返回一个指向结构体的指针。
 */
 
+type Vertex3 struct {
+	X, Y int
+}
+
+
+var (
+	v1 = Vertex3{1,2}   // 创建一个 Vertex 类型的结构体
+	v2 = Vertex3{X:1}    // Y:0 被隐式(implicit)地赋予
+	v3 = Vertex3{}       // X:0 Y:0  直接隐式赋予
+	p  = &Vertex3{1, 2} // 创建一个 *Vertex 类型的结构体（指针）
+)
 func main() {
-	
+	fmt.Println(v1, p, v2, v3)
 }
