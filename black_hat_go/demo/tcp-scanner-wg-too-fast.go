bb3b9f43a664fd9342e47dc34c466b982c5819ff
TesterCC <testerlyx@foxmail.com>
2020-07-01 02:43:54 +0800

    add: pentest demo

--- black_hat_go/demo/tcp-scanner-wg-too-fast.go
+++ black_hat_go/demo/tcp-scanner-wg-too-fast.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-// P26-27  use goroutine
+/*
+P27-28  use WaitGroup from the sync package, is a thread-safe way to control concurrency.
+*/
 func main() {
 	var target = "testphp.vulnweb.com"
 
+	var wg sync.WaitGroup  // step 1
+
 	for i:=1;i<=1024;i++ {
+		wg.Add(1)   // step 2
 		go func(j int){
+			defer wg.Done()   // step 3
 			address := fmt.Sprintf(target+":%d", i)
 			fmt.Println(address)
 			conn, err := net.Dial("tcp", address)
@@ -18,7 +25,8 @@ func main() {
 				return
 			}
 			conn.Close()
-			fmt.Printf("%d open\n",j)  // 不能得到精准port信息，因为主协程没有等待连接完成
+			fmt.Printf("%d open\n",j)
 		}(i)
 	}
+	wg.Wait()  // step4
 }
