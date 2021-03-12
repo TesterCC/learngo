81755738b426c5730c7c78a0dc6e57242acc5e49
TesterCC <testerlyx@foxmail.com>
2021-03-12 15:52:28 +0800

    add:leetcode

---
+++ leetcode/1480_Running-Sum-of-1d-Array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// https://leetcode-cn.com/problems/running-sum-of-1d-array/
+
+var arr = []int{1, 2, 3, 4}   // 注意go的类型约束
+
+func runningSum(nums []int) []int {
+	for i := range nums {
+		if i != 0 {
+			nums[i] = nums[i-1] + nums[i]
+		}
+		fmt.Println(nums)
+	}
+	return nums
+}
+
+func main() {
+	//fmt.Println(arr)
+	runningSum(arr)
+}
