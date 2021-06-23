064a0635798146d81fc671e57507c9a8a817c055
TesterCC <testerlyx@foxmail.com>
2021-06-24 02:15:30 +0800

    fix - word code error, pass testing

--- go_programming_tour/word_convert/cmd/word.go
+++ go_programming_tour/word_convert/cmd/word.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"  // Cobra既是用于创建强大的现代CLI应用程序的库，也是用于生成应用程序和命令文件的程序。
+	"github.com/spf13/cobra" // Cobra既是用于创建强大的现代CLI应用程序的库，也是用于生成应用程序和命令文件的程序。
 	"learngo/go_programming_tour/word_convert/internal/word"
 	"log"
 	"strings"
@@ -10,14 +10,22 @@ import (
 var str string
 var mode int8
 
-
 // 放置单词格式转换子命令word  P22
+
+var desc = strings.Join([]string{
+	"该命令行工具支持各种单词格式转换，模式如下：",
+	"1：全部单词转为大写",
+	"2：全部单词转为小写",
+	"3：下画线单词转为大写驼峰单词",
+	"4：下画线单词转为小写驼峰单词",
+	"5：驼峰单词转为下画线单词",
+}, "\n")
 
 var wordCmd = &cobra.Command{
 	Use:   "word",
 	Short: "单词格式转换",
-	Long:  "支持多种单词格式转换",
-	Run:   func(cmd *cobra.Command, args []string) {
+	Long:  desc,
+	Run: func(cmd *cobra.Command, args []string) {
 		var content string
 		switch mode {
 		case ModeUpper:
@@ -37,7 +45,9 @@ var wordCmd = &cobra.Command{
 	},
 }
 
+// 单词内容和转换的模式进行命令行参数的设置和初始化
 func init() {
+	// 在 VarP 系列的方法中，第一个参数为需绑定的变量、第二个参数为接收该参数的完整的命令标志，第三个参数为对应的短标识，第四个参数为默认值，第五个参数为使用说明。
 	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
 	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "请输入单词转换的模式")
 }
@@ -50,14 +60,3 @@ const (
 	ModeUnderscoreToLowerCamelCase            // 下线线转小写驼峰
 	ModeCamelCaseToUnderscore                 // 驼峰转下划线
 )
-
-var desc = strings.Join([]string{
-	"该命令行工具支持各种单词格式转换，模式如下：",
-	"1：全部单词转为大写",
-	"2：全部单词转为小写",
-	"3：下画线单词转为大写驼峰单词",
-	"4：下画线单词转为小写驼峰单词",
-	"5：驼峰单词转为下画线单词",
-}, "\n")
-
-
--- go_programming_tour/word_convert/main.go
+++ go_programming_tour/word_convert/main.go
@@ -11,3 +11,14 @@ func main() {
 		log.Fatalf("cmd.Excute err: %v", err)
 	}
 }
+
+/*
+基于第三方开源库 Cobra 和标准库 strings、unicode 实现了多种模式的单词转换，非常简单，也是在日常的工作中较实用的一环，因为我们经常会需要对输入、输出数据进行各类型的转换和拼装。
+Testcase:
+go run main.go help word
+go run main.go word -s=TesterCC -m=1
+go run main.go word -s=TesterCC -m=2
+go run main.go word -s=tester_cc -m=3
+go run main.go word -s=tester_cc -m=4
+go run main.go word -s=TesterCC -m=5
+*/
