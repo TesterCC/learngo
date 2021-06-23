a5c8434eaa5cfe93180cd8458cffa1dcbae649a5
TesterCC <testerlyx@foxmail.com>
2021-06-23 21:06:52 +0800

    finish - word_convert coding, but need test

--- go.mod
+++ go.mod
@@ -3,18 +3,14 @@ module learngo
 go 1.13
 
 require (
-	github.com/PuerkitoBio/goquery v1.7.0 // indirect
+	github.com/PuerkitoBio/goquery v1.7.0
 	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
-	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
 	github.com/astaxie/beego v1.12.1
-	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
-	github.com/cpuguy83/go-md2man v1.0.10 // indirect
 	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
-	github.com/shirou/gopsutil v3.21.4+incompatible // indirect
-	github.com/spf13/cobra v1.1.3 // indirect
-	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
-	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
+	github.com/shirou/gopsutil v3.21.4+incompatible
+	github.com/spf13/cobra v1.1.3
+	github.com/tklauser/go-sysconf v0.3.6 // indirect
 	go.mongodb.org/mongo-driver v1.4.4
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
--- go_programming_tour/word_convert/cmd/word.go
+++ go_programming_tour/word_convert/cmd/word.go
@@ -1,6 +1,15 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"  // Cobra既是用于创建强大的现代CLI应用程序的库，也是用于生成应用程序和命令文件的程序。
+	"learngo/go_programming_tour/word_convert/internal/word"
+	"log"
+	"strings"
+)
+
+var str string
+var mode int8
+
 
 // 放置单词格式转换子命令word  P22
 
@@ -8,16 +17,47 @@ var wordCmd = &cobra.Command{
 	Use:   "word",
 	Short: "单词格式转换",
 	Long:  "支持多种单词格式转换",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Run:   func(cmd *cobra.Command, args []string) {
+		var content string
+		switch mode {
+		case ModeUpper:
+			content = word.ToUpper(str)
+		case ModeLower:
+			content = word.ToLower(str)
+		case ModeUnderscoreToUpperCamelCase:
+			content = word.UnderscoreToUpperCamelCase(str)
+		case ModeUnderscoreToLowerCamelCase:
+			content = word.UnderscoreToLowerCamelCase(str)
+		case ModeCamelCaseToUnderscore:
+			content = word.CamelCaseToUnderscore(str)
+		default:
+			log.Fatalf("暂不支持该转换模式，请执行help word查看帮助文档")
+		}
+		log.Printf("Output result: %s", content)
+	},
 }
 
-func init() {}
+func init() {
+	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
+	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "请输入单词转换的模式")
+}
 
 // P26 1.2.4 word 子命令
 const (
-	MODE_UPPER                         = iota + 1 // 全部单词转为大写
-	MODE_LOWER                                    // 全部单词转为小写
-	MODE_UNDERSCORE_TO_UPPER_CAMELCASE            // 下画线单词转为大写驼峰单词
-	MODE_UNDERSCORE_TO_LOWER_CAMELCASE            // 下画线单词转为小写驼峰单词
-	MODE_CAMELCASE_TO_UNDERSCORE                  // 驼峰单词转为下画线单词
+	ModeUpper                      = iota + 1 // 全部转大写
+	ModeLower                                 // 全部转小写
+	ModeUnderscoreToUpperCamelCase            // 下划线转大写驼峰
+	ModeUnderscoreToLowerCamelCase            // 下线线转小写驼峰
+	ModeCamelCaseToUnderscore                 // 驼峰转下划线
 )
+
+var desc = strings.Join([]string{
+	"该命令行工具支持各种单词格式转换，模式如下：",
+	"1：全部单词转为大写",
+	"2：全部单词转为小写",
+	"3：下画线单词转为大写驼峰单词",
+	"4：下画线单词转为小写驼峰单词",
+	"5：驼峰单词转为下画线单词",
+}, "\n")
+
+
