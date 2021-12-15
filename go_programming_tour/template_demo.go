3939dbd4ed62a8343ee2098f86945488faaa580f
TesterCC <testerlyx@foxmail.com>
2021-12-15 13:31:49 +0800

    sync - work notes

--- go_programming_tour/template_demo.go
+++ go_programming_tour/template_demo.go
@@ -5,7 +5,11 @@ import (
 	"strings"
 	"text/template"
 )
-
+/*
+template是Go的文本模版引擎，提供2个标准库
+- text/template: 基于模板输出文本内容
+- html/template: 基于模板输出安全的HTML格式内容，进行了转义，以防注入攻击
+ */
 // template quick start
 const templateText = `
 Output 0: {{title .Name1}}
@@ -15,12 +19,14 @@ Output 2: {{.Name3 | title}}
 
 func main() {
 	funcMap := template.FuncMap{"title":strings.Title}
-	// 创建模板对象   // 1.4.2
+	// 根据给定的名称标识，创建模板对象；调用Parse方法，将常量templateText(即与定义的待解析模板)解析为当前文本模版的主题内容
 	tpl, _ := template.New("go-programming-tour").Funcs(funcMap).Parse(templateText)
 	data := map[string]string{
 		"Name1": "go",
 		"Name2": "programming",
 		"Name3": "tour",
 	}
+
+	// 调用 Execute方法，进行模板渲染，即将传入的data动态参数渲染到对应的模板标识位上
 	_ = tpl.Execute(os.Stdout, data)
 }
--- work_demo/float_demo_test.go
+++ work_demo/float_demo_test.go
@@ -4,8 +4,17 @@ import (
 	"testing"
 )
 
-//go test float_demo_test.go
+// go test float_demo_test.go
 // go test -v float_demo_test.go
+
+/*
+Go的自动化测试框架比JUnit、PyUnit等更加轻量级。
+要点如下：
+- 测试代码以xxx_test.go方式命名
+- 测试代码中import “testing”
+- 测试函数形如 func Testxyz(t *Testing.T) {…}
+- 执行测试：go test
+ */
 
 func TestInit(t *testing.T){
 	p := 9.7
