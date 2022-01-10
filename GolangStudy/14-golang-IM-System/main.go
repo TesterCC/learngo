9cba1b64757fb004911cb7f4857a151bd2b07fc4
TesterCC <testerlyx@foxmail.com>
2022-01-11 01:40:03 +0800

    add - IM-System v0.1

--- GolangStudy/14-golang-IM-System/main.go
+++ GolangStudy/14-golang-IM-System/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+// 程序入口
+/*
+cd ~/14-golang-IM-System
+go build -o server main.go server.go
+./server
 
+在Terminal用nc尝试连接，看服务器返回信息
+nc 127.0.0.1 8888
+ */
+
+func main() {
+	server := NewServer("127.0.0.1", 8888)
+	server.Start()   // main.go和server.go都属于main包，所以不用import了。
 }
--- GolangStudy/14-golang-IM-System/server.go
+++ GolangStudy/14-golang-IM-System/server.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+)
 
+// 服务端 Server基本的listen操作
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// 创建一个Server的接口   返回Server指针
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	//当前连接的业务
+	fmt.Println("连接建立成功")
+}
+
+// 启动服务器的接口
+// 当前类的成员方法
+func (this *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err: ", err)
+		return
+	}
+	// close listen socket
+	defer listener.Close()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err: ", err)
+			continue
+		}
+
+		// do handler
+		go this.Handler(conn) // goroutine 异步协程处理
+	}
 }
