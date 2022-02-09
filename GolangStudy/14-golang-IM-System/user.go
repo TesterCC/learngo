c175f92c88c5f9010667805295f949ca168b74c3
TesterCC <testerlyx@foxmail.com>
2022-02-09 14:36:00 +0800

    add - v0.5

--- GolangStudy/14-golang-IM-System/user.go
+++ GolangStudy/14-golang-IM-System/user.go
@@ -54,9 +54,25 @@ func (this *User) Offline() {
 
 }
 
+// 给当前User对应的客户端发送消息
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
 // 用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	this.server.BroadCast(this, msg)
+	if msg == "who" {
+		// 查询当前在线用户有哪些
+		this.server.mapLock.Lock()
+		for _,user := range this.server.OnlineMap{
+			onlineMsg := "[*]["  + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else {
+		// 正常的消息广播
+		this.server.BroadCast(this, msg)
+	}
 }
 
 
