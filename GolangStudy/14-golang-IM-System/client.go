85d139d4df508549fd7b325ceb21e5891c682e91
TesterCC <testerlyx@foxmail.com>
2022-03-18 14:10:01 +0800

    update - gitignore, v0.9 parse cmd args

--- GolangStudy/14-golang-IM-System/client.go
+++ GolangStudy/14-golang-IM-System/client.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
 
 /*
-v0.9 - 客户端实现 - 链接服务器 p46
+v0.9 - 客户端实现 - 连接服务 p46
 v0.9 - 客户端实现 - 命令行解析 p47
+v0.9 - 客户端实现 - 菜单显示 p48
 
 目标：
 以终端客户端形式实现，实现nc链接server时的功能
@@ -46,9 +48,21 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 }
 
+var serverIp string
+var serverPort int
+
+// useage: ./client -ip 127.0.0.1 -port 8888
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址（默认是127.0.0.1）")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口（默认是8888）")
+}
+
 func main() {
-	serverIp := "127.0.0.1"
-	serverPort := 8888
+
+	// 命令行解析
+	flag.Parse()
+
 	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>> 服务器链接失败 ...")
--- GolangStudy/14-golang-IM-System/main.go
+++ GolangStudy/14-golang-IM-System/main.go
@@ -3,8 +3,14 @@ package main
 // 程序入口
 /*
 cd ~/14-golang-IM-System
-go build -o server main.go server.go
+
+[1] 编译server
+go build -o server main.go server.go user.go
 ./server
+
+[2] 编译client
+go build -o client client.go
+
 
 在Terminal用nc尝试连接，看服务器返回信息
 nc 127.0.0.1 8888
