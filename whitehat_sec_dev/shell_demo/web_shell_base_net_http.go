e109f441856bbf8855f17a15c30dfda41a04c15c
TesterCC <testerlyx@foxmail.com>
2022-11-04 18:25:30 +0800

    fix - print

--- whitehat_sec_dev/shell_demo/web_shell_base_net_http.go
+++ whitehat_sec_dev/shell_demo/web_shell_base_net_http.go
@@ -8,7 +8,7 @@ import (
 // function is just a normal function; it doesn’t have a ServeHTTP() method. So in itself it isn’t a handler.
 // ref: https://lets-go.alexedwards.net/sample/02.09-the-http-handler-interface.html
 func handler (w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("It's works...\n"))   // normal
+	w.Write([]byte("It works...\n"))   // normal
 	//io.WriteString(w, "It's works...\n")
 }
 
