5de8c73d7a1471a9ffe5f8b26a5471f7cbcc6cc9
TesterCC <testerlyx@foxmail.com>
2022-11-28 15:52:45 +0800

    update - .gitignore

--- gopl/ch1/echo/echo3.go
+++ gopl/ch1/echo/echo3.go
@@ -9,5 +9,6 @@ import (
 // P6 use join
 // ./echo3 -v -t -h
 func main() {
-	fmt.Println(strings.Join(os.Args[1:]," "))   // 因为os.Args[0]是命令本身的名字
+	// os.Args[0]是命令本身的名字
+	fmt.Println(strings.Join(os.Args[1:]," "))
 }
