f9c7cc9fe0f9e3486e32f579911132ac4c120470
TesterCC <testerlyx@foxmail.com>
2023-07-26 16:41:15 +0800

    update - mongo crud demo

--- imooc/go_mongodb_crud/crud_demo.go
+++ imooc/go_mongodb_crud/crud_demo.go
@@ -45,8 +45,18 @@ func main() {
 	//insertData := bson.M{"name": "Christine", "gender": "female"}
 	//insertOneDoc(client, ctx, insertData)
 
-	// insert many
-	insertMany(client, ctx)
+	//// insert many
+	//insertMany(client, ctx)
+
+	//// update data
+	//updateData(client, ctx)
+
+	// delete test
+	findData(client, ctx)
+	//deleteData(client, ctx)
+	deleteManyData(client, ctx)
+	fmt.Println("after delete, query again...")
+	findData(client, ctx)
 
 	// disconnect conn
 	defer func() {
@@ -119,13 +129,97 @@ func insertMany(client *mongo.Client, ctx context.Context) {
 
 }
 
-
 // modify data
 func updateData(client *mongo.Client, ctx context.Context) {
 	// connect db and col
 	col := client.Database("test").Collection("insertmanytest")
 
 	// 如果过滤的文档不存在，则插入新的文档
+	updateOneOpts := options.Update().SetUpsert(true)
 
+	// update condition
+	//updateOneFilter := bson.M{"name": "test00"}
+	updateOneFilter := bson.M{"name": "xxx"}
 
+	// after update
+	//updateOneSet := bson.M{"$set": bson.M{"name": "Alfred"}}
+	updateOneSet := bson.M{"$set": bson.M{"name": "Bob"}}
+
+	// update
+	updateResult, err := col.UpdateOne(ctx, updateOneFilter, updateOneSet, updateOneOpts)
+
+	// check error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// output update detail info
+	fmt.Printf("matched: %d ; modified: %d ; upserted: %d ; upsertedID: %v\n",
+		updateResult.MatchedCount,
+		updateResult.ModifiedCount,
+		updateResult.UpsertedCount,
+		updateResult.UpsertedID, // when just update, here is nil
+	)
+
+}
+
+// query mongodb data
+
+func findData(client *mongo.Client, ctx context.Context) {
+	// connect db and col
+	col := client.Database("test").Collection("insertmanytest")
+
+	// set order field, name field, update ascending order
+	findOpts := options.Find().SetSort(bson.D{{"name", 1}})
+
+	// query condition
+	findCursor, err := col.Find(ctx, bson.M{"gender": "male"}, findOpts)
+
+	// define result var
+	var results []bson.M
+
+	// query
+	if err = findCursor.All(ctx, &results); err != nil {
+		log.Fatal(err)
+	}
+
+	// loop for results
+	for _, result := range results {
+		fmt.Println(result)
+	}
+
+}
+
+// delete an item data
+func deleteData(client *mongo.Client, ctx context.Context) {
+	// connect db and col
+	col := client.Database("test").Collection("insertmanytest")
+
+	// delete
+	deleteResult, err := col.DeleteOne(ctx, bson.D{{"name", "test02"}})
+
+	// check error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// output delete lines count
+	fmt.Println("delete count: ", deleteResult.DeletedCount)
+}
+
+// delete many data
+func deleteManyData(client *mongo.Client, ctx context.Context) {
+	// connect db and col
+	col := client.Database("test").Collection("insertmanytest")
+
+	// delete
+	deleteManyResult, err := col.DeleteMany(ctx, bson.D{{"name", "test02"}})
+
+	// check error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// output delete lines count
+	fmt.Println("delete count: ", deleteManyResult.DeletedCount)
 }
