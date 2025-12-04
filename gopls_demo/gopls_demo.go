40c219df2d8239613ed5a940d7bac9ccc5b6f172
TesterCC <testerlyx@foxmail.com>
2025-12-04 15:25:33 +0800

    add - gopls simple notes

---
+++ gopls_demo/gopls_demo.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+
+
+https://go.dev/gopls/
+
+这份文档（https://go.dev/gopls）描述了 gopls 的最新版本
+
+Gopls 支持多种标准 LSP 功能，包括导航、补全、诊断、分析和重构，以及其他语言服务器中未提供的许多附加功能。
+
+Gopls: 设置你的工作区
+https://go.dev/gopls/workspace
+
+查看功能索引以获取有关 Gopls 能为您做什么的完整文档。
+https://go.dev/gopls/features/  最重要的使用文档
+
+
+
+Gopls: The language server for Go
+
+Gopls: Advanced topics
+https://go.dev/gopls/advanced#installing-unreleased-versions
+
+
+*/
